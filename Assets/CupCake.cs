2fe4bf83210ddda7b9cb6015b17ae1cd961cf9a1
VDI\A110082015 <charlotte082015@gmail.com>
2022-10-23 00:51:30 +0800

    增加角色控制

--- Assets/CupCake.cs
+++ Assets/CupCake.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CupCake : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CharacterController controller;
+    public float speed = 3;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+
+        if (dir.magnitude > 0.1f)
+        {
+            float faceAngle = Mathf.Atan2(h, v) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0, faceAngle, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 0.3f);
+        }
+
+        Vector3 move = dir * speed;
+
+        if (!controller.isGrounded)
+        {
+            move.y = -9.8f * 100 * Time.deltaTime;
+        }
+
+        controller.Move(move * speed * Time.deltaTime);
     }
+
 }
